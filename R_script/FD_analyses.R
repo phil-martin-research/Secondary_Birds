f9bac83e83d8c8cf51332fd589619f63fb4eb805
Phil Martin <philip.martin.1981@gmail.com>
2014-08-07 21:50:28 +0200

    small edites to FD calculation

--- R_script/FD_analyses.R
+++ R_script/FD_analyses.R
@@ -17,6 +17,8 @@ dummy$trait #dataframe of 8 functional traits for 8 species
 dummy$abun # matrix of abundance of 8 species in 10 communities
 
 ex<-dbFD(dummy$trait,dummy$abun)
+?dbFD
+
 
 #traits
 #1. diets in form of factors
@@ -32,22 +34,28 @@ ex<-dbFD(dummy$trait,dummy$abun)
 # species - contains duplicate rows of species and traits in binary form
 x<-getURL("https://raw.githubusercontent.com/PhilAMartin/Secondary_Birds/master/Data/Feeding%20guilds%2011.12.13%20v1.csv",ssl.verifypeer = FALSE)
 species<-read.csv(textConnection(x),header=T)
-names(species)
+head(species)
+
+#create species by site matrix, species as columns sites as row names
+Sp_melt<-melt(species,id.vars =c("Scientific.name","SiteID"))
+head(Sp_melt)
+?cast
+cast(Trait_melt,value~Scientific.name)
+
 
 # traits2 - contains only unique rows (1785 species) and traits in binary form
 traits2<-subset(species, !duplicated(Taxon.ID))
 names(traits2)
-rownames(traits2)<-traits2[,2] #set TaxonID as row names
-traits2[,2] <- NULL #remove Taxon ID column
+rownames(traits2)<-gsub(" ", ".", traits2$Scientific.name)#set TaxonID as row names
 
 #select just columns that have trait data in
-traits3<-head(traits2)[,c(3:7)]
+traits3<-traits2[,c(3:7)]
+traits3<-traits3[order(row.names(traits3)),]
 
 
 #traits2=x with 7 traits (=food types) with binary code (form 2 - see above)
 
 #create presence absence matrix with site as a row and species as a column
-
 
 #sites2 - presence/absence data
 x2<-getURL("https://raw.githubusercontent.com/PhilAMartin/Secondary_Birds/master/Data/Site_PresAb.csv",ssl.verifypeer = FALSE)
@@ -55,6 +63,10 @@ sites2<-read.csv(textConnection(x2),header=T)
 head(sites2)
 rownames(sites2)<-sites2[,1] #set SiteID as row names
 sites2[,1]<-NULL #remove SiteID column
+#reorder these so that the columns are in ascending alphabetical order 
+sites2<-sites2[,order(names(sites2))]
+
+
 
 #sites2=a with presence abundance data (form B - see above)
 # for pres/ab, w.abun=FALSE
@@ -65,24 +77,22 @@ sites2[,1]<-NULL #remove SiteID column
 #FEve = functional eveness
 #FDiv = functional diversity - set calc.FDiv=TRUE.
 
-?dbFD
-head(traits3)
 
-head(sites2)
-
-#the traits data needs flipping around and all data apart from traits need to be removed
-Trait_melt<-melt(traits2)
-head(Trait_melt)
-?cast
-cast(Trait_melt,value~Scientific.name)
-
+names(sites2)
+row.names(traits3)
+traits3
 
 #2B.1
-dbFD(x=traits2,a=sites2,w.abun=FALSE,calc.FRic=TRUE,calc.FDiv=TRUE)
+
+sites2<-sites2[1:4,]
+
+dbFD(x=traits3,a=sites2,w.abun=FALSE,calc.FRic=TRUE,calc.FDiv=F,asym.bin=NULL)
 
 # gives error - add asym.bin (unsure how to apply)
 #2B.2
-dbFD(x=traits2,a=sites2,w.abun=FALSE,asym.bin=c(1,2,3,4,5,6,7),calc.FRic=TRUE,calc.FDiv=TRUE)
+dbFD(x=traits2,a=sites2)
+
+,w.abun=FALSE,asym.bin=c(1,2,3,4,5,6,7),calc.FRic=TRUE,calc.FDiv=TRUE)
 # OR are binary traits asymmetric? If not, add asym.bin as null
 dbFD(x=traits2,a=sites2,w.abun=FALSE,asym.bin=NULL,calc.FRic=TRUE,calc.FDiv=TRUE)
 
