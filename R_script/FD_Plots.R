6ee6091bcddd46293d23d735ae2221d8626ca60e
Phil Martin <philip.martin.1981@gmail.com>
2015-09-23 15:59:19 +0100

    produced new plots of functional diversity & richness

--- R_script/FD_Plots.R
+++ R_script/FD_Plots.R
@@ -2,8 +2,8 @@ rm(list=ls())
 
 # load data
 setwd("C:/Users/Catherine/Dropbox/Phil & Catherine - Secondary forests/Analysis - Dec 2013/Data")
-data<-read.csv("FD Data To Use.csv",header=T)
-dataAb<-read.csv("FD_Abun Data To Use.csv",header=T)
+sf_data<-read.csv("Data/FD Data To Use.csv",header=T)
+dataAb<-read.csv("Data/FD_Abun Data To Use.csv",header=T)
 
 library(nlme)
 
@@ -11,7 +11,7 @@ library(nlme)
 # PLOTS with data2 - some sites removed
   
 # remove sites without continuity with PF data
-data2<-subset(data,data$PFCont!="")
+data2<-subset(sf_data,PFCont!="")
 
 # SSR - null model
 
@@ -66,11 +66,18 @@ summary(AbFdiv7) # -ve AIC, +Ve logLik
 library(ggplot2)
 #create dataframe
 FD_df<-data.frame(
-  metric=c("FDiv","FRic","SSR"),
+  metric=c("Functional diversity","Functional richness","Single species richness"),
   mean=c(0.9972861,1.094123,0.984084),
   se=c(0.01028214,0.1362707,0.03589116))
 #define top and bottom of error bars
 limits <- aes(ymax = mean+(1.96*se), ymin=mean-(1.96*se))
+
+theme_set(theme_bw(base_size=12))
 p <- ggplot(FD_df, aes(y=mean,x=metric))
-p + geom_pointrange(limits) + labs(x = "Metric", y="Mean value in secondary forest relative to primary forest with 95% Confidence Intervals") + 
-  ylim(0.6,1.6) + geom_hline(linetype="dashed",aes(yintercept=1)) +theme_bw()
+p2<-p + geom_pointrange(limits,shape=1) + labs(x = "Metric", y="Value in secondary forest relative\n to primary forest") + 
+  ylim(0.6,1.6) + geom_hline(linetype="dashed",aes(yintercept=1))
+p2+theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_rect(size=1.5,colour="black",fill=NA))
+ggsave(filename = "Figures/Functional_div.pdf",width = 6,height=4,units='in',dpi=400)
+ggsave(filename = "Figures/Functional_div.png",width = 6,height=4,units='in',dpi=400)
