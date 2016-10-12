ab7956e13317b807c0bd63b86377dbda432ec22c
Phil Martin <phil.martin.research@gmail.com>
2016-10-12 10:44:15 +0100

    removed loop that calculated FD using presence/absence as we do not use this in paper

--- R_script/FD_calculations.R
+++ R_script/FD_calculations.R
@@ -27,60 +27,6 @@ row.names(Traits)<-gsub(" ", ".", Traits$Scientific.Name, fixed = TRUE)#put dot 
 Traits2<-Traits[-c(1:4)]#remove columns that are not needed from trait file
 Traits3<-data.matrix(Traits2)#convert trait data to a data.matrix
 
-#create a list of all the studies we are using
-Unique_study<-unique(P_ab$Study)
-
-#first calculate presence/absence statistics
-#for the moment this is only using Petchy and GAston's method
-#it might eb useful to come up with another method to calculate functional diversity
-FD_summary<-NULL
-Unique_study<-unique(P_ab$Study)
-for (i in 1:length(Unique_study)){
-  PA_sub<-subset(P_ab,Study==Unique_study[i])#subset data so that it only represents one study
-  Sites<-unique(PA_sub$Site.ID)#get site ID numbers
-  PF_SF<-PA_sub$PF_SF#Get info on whether sites are secondary or primary forest
-  Age<-PA_sub$Age#get info on site ages
-  Methods<-PA_sub[,5:8]#get info on the methods used in studies
-  PA_sub2<-PA_sub[-c(1:8)]#remove columns that indicate site, study number, whether they are primary or secondary, and methods used in study
-  keeps<-colSums(PA_sub2)>0#identify species not present in local species pool
-  PA_sub3<-PA_sub2[,keeps]#get rid of species which are not present in local species pool
-  PA_sub3<-PA_sub3[ , order(names(PA_sub3))]#order columns for species alphabetically
-  Trait_sp2<-data.frame(Species=row.names(Traits3))#create a dataframe with one column containing species names for which we have traits
-  Trait_sp2$Match<-row.names(Traits3) %in% names(PA_sub3)#mark species as "TRUE" if we have details of them in sites and "FALSE" if we do not
-  remove_sp2<-subset(Trait_sp2,Match=="FALSE")[,1]#produce vector of species to remove from dataset
-  Trait_ab2<-Traits3[-which(rownames(Traits3) %in% remove_sp2), ]#remove species from trait dataset
-  Trait_ab2<-Trait_ab2[order(rownames(Trait_ab2)), ]#order trait dataset so it has the same order as the species dataset
-  FD_dendro_summary<-FD_dendro(S=Trait_ab2, A=PA_sub3,Cluster.method = "average", ord = "podani",Weigthedby = "abundance")
-  FD_summary_study<-dbFD(Trait_ab2, PA_sub3, corr="sqrt",w = c(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,#produce fd metrics, giving all four traits a similar weight
-                  1/7,1/7,1/7,1/7,1/7,1/7,1/7,1,1),w.abun = FALSE,calc.FRic=T,m=10)
-  FD_site<-data.frame(Site=Sites,Study=Unique_study[i],PF_SF=PF_SF,Age=Age,Methods,SpR=FD_dendro_summary$n_sp,FDpg=FD_dendro_summary$FDpg,
-                      FRic=FD_summary_study$FRic,qual_FRic=FD_summary_study$qual.FRic,FEve=FD_summary_study$FEve,
-                      FDiv=FD_summary_study$FDiv,FDis=FD_summary_study$FDis,RaoQ=FD_summary_study$RaoQ)
-  FD_site_PF<-subset(FD_site,PF_SF=="PF")#subset to give only primary forest sites
-  FD_site_SF<-subset(FD_site,PF_SF=="SF")#subset to give only secondary forest sites
-  #include diversity metrics for primary forest reference sites
-  FD_site_SF$PF_SpR<-FD_site_PF$SpR
-  FD_site_SF$PF_FDpg<-FD_site_PF$FDpg
-  FD_site_SF$PF_FRic<-FD_site_PF$FRic
-  FD_site_SF$PF_FEve<-FD_site_PF$FEve
-  FD_site_SF$PF_FDiv<-FD_site_PF$FDiv
-  FD_site_SF$PF_FDis<-FD_site_PF$FDis
-  FD_site_SF$PF_RaoQ<-FD_site_PF$RaoQ
-  #now calculate the log response ratio effect size as a measure of difference between secondary and primary sites
-  FD_site_SF$SpR_comp<-log(FD_site_SF$SpR)-log(FD_site_PF$SpR)
-  FD_site_SF$FDpg_comp<-log(FD_site_SF$FDpg)-log(FD_site_PF$FDpg)
-  FD_site_SF$FR_comp<-log(FD_site_SF$FRic)-log(FD_site_PF$FRic)
-  FD_site_SF$FE_comp<-log(FD_site_SF$FEve)-log(FD_site_PF$FEve)
-  FD_site_SF$FDiv_comp<-log(FD_site_SF$FDiv)-log(FD_site_PF$FDiv)
-  FD_site_SF$FDis_comp<-log(FD_site_SF$FDis)-log(FD_site_PF$FDis)
-  FD_site_SF$Rao_comp<-log(FD_site_SF$RaoQ)-log(FD_site_PF$RaoQ)
-  FD_summary<-rbind(FD_site_SF,FD_summary)
-  print(i)
-}
-
-write.csv(FD_summary,"Data/FD_summary_comp.csv",row.names=F)
-
-
 #now calculate statistics for data including species abundance
 #for this I need to produce FD statistics for one site at a time
 #probably best to use a loop to achieve this
